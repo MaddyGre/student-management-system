9cbe11698e2958170491c4441ed8f2147148c990
Maddalena Onyehara <maddalenaonyehara@gmail.com>
2021-03-16 16:25:56 +0000

    Latest working version with validation included

--- src/main/java/com/maddy/controller/StudentController.java
+++ src/main/java/com/maddy/controller/StudentController.java
@@ -3,9 +3,18 @@ package com.maddy.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+=======
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+>>>>>>> b4bf349 (Uploading the latest working version with validation included)
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,8 +40,12 @@ public class StudentController {
 	public String viewHomePage(Model model) {
 		List<Student> listStudents = service.listAll();
 		model.addAttribute("listStudents", listStudents);
+<<<<<<< HEAD
 		
 		
+=======
+			
+>>>>>>> b4bf349 (Uploading the latest working version with validation included)
 		return "index";
 	}
 	
@@ -45,10 +58,23 @@ public class StudentController {
 	}
 	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
+<<<<<<< HEAD
 	public String saveStudent(@ModelAttribute("student") Student student) {
 		service.save(student);
 		
 		return "redirect:/";
+=======
+	public String saveStudent(@Valid @ModelAttribute("student") Student student,
+			BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) {
+			return "new_student";
+		} else {
+			service.save(student);
+			return "redirect:/";
+		}
+			
+>>>>>>> b4bf349 (Uploading the latest working version with validation included)
 	}
 	
 	@RequestMapping("/student/edit/{studentId}")
--- src/main/java/com/maddy/entity/Student.java
+++ src/main/java/com/maddy/entity/Student.java
@@ -1,14 +1,30 @@
 package com.maddy.entity;
 
+<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.List;
+=======
+>>>>>>> b4bf349 (Uploading the latest working version with validation included)
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+<<<<<<< HEAD
 
 @Entity
+=======
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@DynamicUpdate
+>>>>>>> b4bf349 (Uploading the latest working version with validation included)
 public class Student {
 	
 	@Id
@@ -17,6 +33,7 @@ public class Student {
 	private Long studentId;
 	
 	@Column(name="firstname")
+<<<<<<< HEAD
 	private String firstName;
 	
 	@Column(name="lastname")
@@ -30,6 +47,28 @@ public class Student {
 	
 	@Column(name="tuitionbalance")
 	private double tuitionBalance;
+=======
+	@Size(min = 1, max = 20, message = "First name cannot be empty")
+	private String firstName;
+	
+	@Column(name="lastname")
+	@Size(min = 1, max = 20, message = "Last name cannot be empty")
+	private String lastName;
+	
+	@Column(name="gradeyear")
+	@NotNull(message = "Please enter grade year in this format: XXXX")
+	@Min(4)
+	private Integer gradeYear;
+	
+	@Column(name="course")
+	@Size(min = 1, max = 15, message = "Course cannot be empty")
+	private String course;
+	
+	@Column(name="tuitionbalance")
+	@NotNull(message = "Please enter tuition balance")
+	@Min(4)
+	private Double tuitionBalance = 0.0;
+>>>>>>> b4bf349 (Uploading the latest working version with validation included)
 	
 	public Student() {
 	
@@ -62,6 +101,7 @@ public class Student {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+<<<<<<< HEAD
 
 	public int getGradeYear() {
 		return gradeYear;
@@ -71,10 +111,22 @@ public class Student {
 		this.gradeYear = gradeYear;
 	}
 	
+=======
+	
+	public Integer getGradeYear() {
+		return gradeYear;
+	}
+
+	public void setGradeYear(Integer gradeYear) {
+		this.gradeYear = gradeYear;
+	}
+
+>>>>>>> b4bf349 (Uploading the latest working version with validation included)
 	public String getCourse() {
 		return course;
 	}
 
+<<<<<<< HEAD
 
 	public void setCourse(String course) {
 		this.course = course;
@@ -86,6 +138,17 @@ public class Student {
 	}
 
 	public void setTuitionBalance(double tuitionBalance) {
+=======
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	
+	public Double getTuitionBalance() {
+		return tuitionBalance;
+	}
+
+	public void setTuitionBalance(Double tuitionBalance) {
+>>>>>>> b4bf349 (Uploading the latest working version with validation included)
 		this.tuitionBalance = tuitionBalance;
 	}
 
